077923fe7dc7931b15c553f09d175635f4a58065
Adam Pearce <1wheel@gmail.com>
2017-11-15 14:00:07 -0500

    new sel arg

--- _script.js
+++ _script.js
@@ -1,10 +1,10 @@
 console.clear()
-
 d3.select('body').selectAppend('div.tooltip')
 
 var data = d3.range(300).map(x => { return {x, y: Math.random()*10 + x/100} })
 
-var c = d3.conventions({parentSel: d3.select('.graph').html(''), margin: {left: 30}})
+var sel = d3.select('.graph').html('')
+var c = d3.conventions({sel, margin: {left: 30}})
 
 c.x.domain(d3.extent(data, d => d.x))
 c.y.domain(d3.extent(data, d => d.y))
